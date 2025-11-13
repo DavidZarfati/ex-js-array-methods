3af62621b627a6c3996d23c8997c7f7de84d26c3
DavidZarfati <zarda2001@gmail.com>
2025-11-13 15:16:52 +0100

    feat:snack 4, con function e map

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,7 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+const nomiAutori = posts.map(function ({ author }) {
+  return author
+})
+console.log(nomiAutori);
